39772c5f83e107735a2201bebc00f0b6a4e731d1
ZenonStella <stellazenon@gmail.com>
2022-06-13 13:19:45 +0200

    first commit

---
+++ assets/data/glaces.php
@@ -0,0 +1,33 @@
+<?php
+$glaces = [
+    [
+        'img' => 'fruits-rouges',
+        'choises' => 'fruits rouges en forme de fleure',
+        'prices' => 2.99,
+        'description' => 'En été, ou même toute l\'année, faites-vous une fleur avec ces délicieux cornets glacés aux fruits rouges.'
+    ],
+    [
+        'img' => 'cafe',
+        'choises' => 'café',
+        'prices' => 2.75,
+        'description' => 'Succombez aux bienfaits du végétal, et aux arômes du café de Colombie avec ces bâtonnets gourmands.'
+    ],
+    [
+        'img' => 'framboise',
+        'choises' => 'citron-framboise',
+        'prices' => 2.99,
+        'description' => 'La gourmandise n\'a jamais été aussi rafraîchissante, surtout quand le citron se marie avec la framboise.'
+    ],
+    [
+        'img' => 'batonnet-fruits',
+        'choises' => 'fruits',
+        'prices' => 2.99,
+        'description' => 'Des bâtonnets glacés aux fruits, et avec seulement le sucre des fruits, un délice pour les enfants, et une bonne nouvelle pour les parents !'
+    ],
+    [
+        'img' => 'orange-carotte-citron',
+        'choises' => 'orange carotte et citron',
+        'prices' => 2.99,
+        'description' => 'La surprise des beaux jours c\'est découvrir de nouvelles saveurs grâce au cocktail orange carotte et citron en bâtonnets glacés.'
+    ]
+];
